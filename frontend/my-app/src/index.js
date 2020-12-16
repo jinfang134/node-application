211c2f6de2f08f209857cb6b781dd883342063d9
lmeng1 <lmeng1@paypal.com>
2020-12-16 18:09:24 +0800

    Add layout

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Sign from './Sign'
-
 ReactDOM.render(
+  // <div className="App">
+  // <h1>antd version: {version}</h1>
+  // <DatePicker />
+  // <Button type="primary" style={{ marginLeft: 8 }}>
+  // Primary Button
+  // </Button>
+  // </div>,
   <React.StrictMode>
-    <Sign />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
